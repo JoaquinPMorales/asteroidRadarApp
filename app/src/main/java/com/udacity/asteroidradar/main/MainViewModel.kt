e408fcfe654e614656724dafd46225625a2dec94
JoaquinPMorales <joaquin.perez@partner.bmw.de>
2022-09-10 20:22:25 +0200

    Added viewModel and ViewModelFactory with databinding for MainViewModel

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,11 @@
 package com.udacity.asteroidradar.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.udacity.asteroidradar.database.AsteroidDatabase
+import com.udacity.asteroidradar.database.AsteroidDatabaseDao
 
-class MainViewModel : ViewModel() {
+class MainViewModel(val database: AsteroidDatabaseDao,
+                    application: Application) : AndroidViewModel(application) {
+
 }
